019ecf616574cb2f1e17a0f6f9f029a84581c1b7
childressdev <jcampbell@childressagency.com>
2018-02-26 11:56:21 -0500

    updated

--- page-services.php
+++ page-services.php
@@ -5,6 +5,7 @@
         <div class="col-sm-5 col-md-4">
           <nav id="services-nav">
             <?php
+              $current_page_id = get_the_ID();
               $services_cats = get_terms(array(
                 'taxonomy' => 'services_categories',
                 'parent' => 0,
@@ -30,79 +31,35 @@
                         )
                       ));
 
-                      if($parent_services->have_posts()): while($parent_services->have_posts()): $parent_services->the_post(); ?>
-                        <ul class="nav sidebar-nav">
-
-                        </ul>
+                      if($parent_services->have_posts()): while($parent_services->have_posts()): $parent_services->the_post();
+                        $parent_page_id = get_the_ID(); ?>
+                        <li<?php if($parent_page_id == $current_page_id){ echo ' class="active"'; } ?>>
+                          <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+                          <?php 
+                            $children = wp_list_pages(array(
+                              'child_of' => $parent_page_id, 
+                              'post_type' => 'services_cpt',
+                              'echo' => false,
+                            ));
+                            if($children) : ?>
+                              <ul>
+                                <?php echo $children; ?>
+                              </ul>
+                          <?php endif; ?>
+                        </li>
                     <?php endwhile; endif; wp_reset_postdata(); ?>
                   </ul><?php //sidebar-nav ?>
                 <?php endforeach; ?>
               <?php endif; //service_cats ?>
             </nav>
-            <ul class="nav sidebar-nav">
-              <li class="resource">
-                <a href="#">Financial Calculators</a>
-                <ul class="nav resources-sub-nav">
-                  <li class="active">
-                    <a href="#">Cash Flow</a>
-                    <ul class="nav resources-sub-sub-nav">
-                      <li><a href="#">How does inflation impact my standard of living?</a></li>
-                      <li><a href="#">What is my projected cashflow?</a></li>
-                      <li><a href="#">What is the value of reducing, postponing or foregoing expenses?</a></li>
-                      <li><a href="#">Historical inflation - compare purchasing power</a></li>
-                      <li><a href="#">How much am I spending?</a></li>
-                      <li><a href="#">How much do I need for emergencies?</a></li>
-                      <li><a href="#">Should I pay down debt or invest my monthly surplus?</a></li>
-                    </ul>
-                  </li>
-                  <li>
-                    <a href="#">College</a>
-                    <ul class="nav resources-sub-sub-nav">
-                      <li><a href="#">How much should I be saving for college?</a></li>
-                      <li><a href="#">Feasibility of student loan repayment</a></li>
-                      <li><a href="#">What are the advantages of a Coverdell ESA?</a></li>
-                      <li><a href="#">What are the advantages of a 529 college savings plan?</a></li>
-                      <li><a href="#">What is the value of a college education?</a></li>
-                    </ul>
-                  </li>
-                  <li>
-                    <a href="#">Credit</a>
-                    <ul class="nav resources-sub-sub-nav">
-                      <li><a href="#">How long will it take to pay off my credit card?</a></li>
-                      <li><a href="#">Should I pay off debt or invest?</a></li>
-                      <li><a href="#">Should I transfer my credit card balances?</a></li>
-                      <li><a href="#">Credit score estimator</a></li>
-                      <li><a href="#">How long until my loan is paid off?</a></li>
-                      <li><a href="#">What would my loan payments be?</a></li>
-                    </ul>
-                  </li>
-                  <li>
-                    <a href="#">Home and Mortgage</a>
-                    <ul class="nav resources-sub-sub-nav">
-                      <li><a href="#">How much home can I afford?</a></li>
-                      <li><a href="#">Which is better: Fixed or Adjustable-Rate mortgage?</a></li>
-                      <li><a href="#">Adjustable rate mortgage calculator</a></li>
-                      <li><a href="#">How do closing costs impact the interest rate?</a></li>
-                    </ul>
-                  </li>
-                </ul>
-              </li>
-              <li class="resource">
-                <a href="#">Resource Name 2</a>
-                <ul class="nav resources-sub-nav">
-                  <li><a href="#">Resource Sub Name 1</a></li>
-                  <li><a href="#">Resource Sub Name 2</a></li>
-                </ul>
-              </li>
-            </ul>
-          </nav>
-        </div>
+          </div>
         <div class="col-sm-7 col-md-8">
           <article>
-            <h1>Resource introduction title</h1>
-            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.</p>
-            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.</p>
-            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.</p>
+            <?php 
+              if(have_posts()): while(have_posts()): the_post();
+                the_content(); 
+              endwhile; endif; 
+            ?>
           </article>
         </div>
       </div>
